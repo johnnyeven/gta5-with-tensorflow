21b798bb762bd018ae01650b868e146431f41665
liyiwen <liyiwen@huoyunren.com>
2019-07-24 21:23:45 +0800

    init

--- inputs_test.py
+++ inputs_test.py
@@ -1,17 +1,25 @@
-# from modules.key_inputs import GTA5KeyboardEvent
-#
-#
-# def key_change_handler(keys):
-#     print("\r", keys, end='', flush=True)
-#
-#
-# evt = GTA5KeyboardEvent(key_change_handler)
-# evt.run()
+from modules.key_inputs import GTA5KeyboardEvent
 
-from inputs import get_key
 
-while True:
-    events = get_key()
-    for event in events:
-        if event.ev_type == "Key":
-            print("\r", event.code, end='')
+def key_change_handler(keys):
+    print("\r", keys, end='', flush=True)
+
+
+def main():
+    evt = GTA5KeyboardEvent(key_change_handler)
+    evt.run()
+
+
+# from inputs import get_gamepad
+#
+#
+# def main():
+#     while 1:
+#         events = get_gamepad()
+#         if events:
+#             for event in events:
+#                 print(event.ev_type, event.code, event.state)
+#
+#
+if __name__ == "__main__":
+    main()
